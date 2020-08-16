7e63108d98249877ae22e12a995d6a4e5a3b0f34
AQadir64 <a.qadir67@outlook.com>
2020-08-16 12:03:01 +0500

    added : reusable addLocation hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
+
+export default (shouldTrack, callback) => {
+  const [err, setErr] = useState(null);
+  const [subscriber, setSubsciber] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      const sub = await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+      setSubsciber(sub);
+    } catch (error) {
+      setErr(error);
+    }
+  };
+  useEffect(() => {
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubsciber(null);
+    }
+  }, [shouldTrack]);
+
+  return [err];
+};
