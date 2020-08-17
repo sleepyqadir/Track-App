44620992050ab5be86f20011df295c49307699c1
AQadir64 <a.qadir67@outlook.com>
2020-08-17 11:49:11 +0500

    update : useEffect clenaup

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -6,33 +6,37 @@ import {
 } from "expo-location";
 
 export default (shouldTrack, callback) => {
-  
   const [err, setErr] = useState(null);
-  const [subscriber, setSubsciber] = useState(null);
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubsciber(sub);
-    } catch (error) {
-      setErr(error);
-    }
-  };
+
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        await requestPermissionsAsync();
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+      } catch (error) {
+        setErr(error);
+      }
+    };
     if (shouldTrack) {
       startWatching();
     } else {
       subscriber.remove();
-      setSubsciber(null);
+      subscriber = null;
     }
-  }, [shouldTrack,callback]);
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback, subscriber]);
 
   return [err];
 };
